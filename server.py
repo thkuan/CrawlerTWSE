8c26f829168f1bf03675885a20ccd9c457291716
thkuan <kuanken.cs96@g2.nctu.edu.tw>
2017-10-12 14:38:47 +0800

    New Info. added

--- server.py
+++ server.py
@@ -12,6 +12,6 @@ def hello():
 @app.route("/resp")
 def get_resp():
     if request.method == 'GET':
-        query_type = request.args.get('s_type')
+        query_type = request.args.get('q_option')
         query_company = request.args.get('codename')
         return render_template('form.html', parsing_result=retreive(query_type, query_company))
--- twse.py
+++ twse.py
@@ -3,44 +3,102 @@ import requests
 import urllib
 import bs4
 
-def inquiry_web(string):
+def inquiry_web(key):
     web_page = {
-        'eps': 'http://mops.twse.com.tw/mops/web/ajax_t05st09',
         'basic_info': 'http://mops.twse.com.tw/mops/web/ajax_t05st03',
+        'dividend_distr': 'http://mops.twse.com.tw/mops/web/ajax_t05st09',
+        'yearly_eps': 'http://mops.twse.com.tw/mops/web/ajax_t05st22',
+        'quarterly_eps': 'http://mops.twse.com.tw/mops/web/ajax_t163sb15',
     }
 
-    return web_page[string]
+    return web_page[key]
 
-def get_payload(code_name):
+def get_payload(key, code_name):
     payload = {
-        'encodeURIComponent': '1',
-        'step': '1',
-        'firstin': '1',
-        'off': '1',
-        'keyword4': '',
-        'code1': '',
-        'TYPEK2': '',
-        'checkbtn': '',
-        'queryName': 'co_id',
-        'inpuType': 'co_id',
-        'TYPEK': 'all',
-        'co_id': code_name,
+        'basic_info':
+        {
+            'encodeURIComponent': '1',
+            'step': '1',
+            'firstin': '1',
+            'off': '1',
+            'keyword4': '',
+            'code1': '',
+            'TYPEK2': '',
+            'checkbtn': '',
+            'queryName': 'co_id',
+            'inpuType': 'co_id',
+            'TYPEK': 'all',
+            'co_id': code_name,
+        },
+        'dividend_distr':
+        {
+            'encodeURIComponent': '1',
+            'step': '1',
+            'firstin': '1',
+            'off': '1',
+            'keyword4': '',
+            'code1': '',
+            'TYPEK2': '',
+            'checkbtn': '',
+            'queryName': 'co_id',
+            'inpuType': 'co_id',
+            'TYPEK': 'all',
+            'isnew': 'true',
+            'co_id': code_name,
+            'year': '',
+        },
+        'yearly_eps':
+        {
+            'encodeURIComponent': '1',
+            'run': 'Y',
+            'step': '1',
+            'TYPEK': 'sii',
+            'year': '105',
+            'isnew': 'true',
+            'co_id': code_name,
+            'firstin': '1',
+            'off': '1',
+            'ifrs': 'Y',
+        },
+        'quarterly_eps':
+        {
+            'encodeURIComponent': '1',
+            'step': '1',
+            'firstin': '1',
+            'off': '1',
+            'keyword4': '',
+            'code1': '',
+            'TYPEK2': '',
+            'checkbtn': '',
+            'queryName': 'co_id',
+            't05st29_c_ifrs': 'N',
+            't05st30_c_ifrs': 'N',
+            'inpuType': 'co_id',
+            'TYPEK': 'all',
+            'isnew': 'true',
+            'co_id': code_name,
+            'year': '',
+        },
     }
 
     if sys.version_info >= (3,):
-        return urllib.parse.urlencode(payload)
+        return urllib.parse.urlencode(payload[key])
     else:
-        return urllib.urlencode(payload)
+        return urllib.urlencode(payload[key])
 
 #if __name__ == '__main__':
-def retreive(t, code_name=6414):
-    translate = {
-        'EPS': 'eps',
+def retreive(key, code_name=6414):
+    dict_maps = {
         'BASIC': 'basic_info',
+        'DIVIDEND': 'dividend_distr',
+        'YEARLY EPS': 'yearly_eps',
+        'QUARTERLY EPS': 'quarterly_eps',
+        'PE RATIO': 'basic_info',
     }
+    key_val = dict_maps[key]
     s = requests.Session()
-    payload = get_payload(code_name)
-    resp = s.post(inquiry_web(translate[t]), data=payload)
+    payload = get_payload(key_val, code_name)
+    resp = s.post(inquiry_web(key_val), data=payload)
     resp.encoding = 'utf-8'
     if resp.ok:
         return resp.text
