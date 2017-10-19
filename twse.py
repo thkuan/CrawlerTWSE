d6aa79c09ed7d0376ac539855097d77446e78368
thkuan <kuanken.cs96@g2.nctu.edu.tw>
2017-10-19 18:49:10 +0800

    Reorgnize crawler arch

--- twse.py
+++ twse.py
@@ -3,13 +3,16 @@ import requests
 import urllib
 from bs4 import BeautifulSoup
 
-dict_maps = {
-    'BASIC': 'basic_info',
-    'DIVIDEND': 'dividend_distr',
-    'YEARLY EPS': 'yearly_eps',
-    'QUARTERLY EPS': 'quarterly_eps',
-    'PE RATIO': 'basic_info',
-    'NET PROFIT PLUS EPS': 'yearly_eps',
+DICT_MAPS = {
+    # 'Key': a list of sub-keys
+    'BASIC': ['basic_info', 'type_A'],
+    'DIVIDEND': ['dividend_distr', 'type_B'],
+    'YEARLY EPS': ['yearly_eps', 'type_C'],
+    'QUARTERLY EPS': ['quarterly_eps', 'type_D'],
+    'PE RATIO': ['basic_info', 'type_A'],
+    'NET PROFIT PLUS EPS': ['quarterly_eps', 'type_D'],#['yearly_eps', 'type_C'],
+    'PROFIT ANALYSIS': ['profit_analysis', 'type_D'],
+    'OPEX': ['opex', 'type_D'],
 }
 
 def inquiry_web(key):
@@ -18,13 +21,16 @@ def inquiry_web(key):
         'dividend_distr': 'http://mops.twse.com.tw/mops/web/ajax_t05st09',
         'yearly_eps': 'http://mops.twse.com.tw/mops/web/ajax_t05st22',
         'quarterly_eps': 'http://mops.twse.com.tw/mops/web/ajax_t163sb15',
+        'profit_analysis': 'http://mops.twse.com.tw/mops/web/ajax_t163sb08',
+        'opex': 'http://mops.twse.com.tw/mops/web/ajax_t163sb09',
     }
 
-    return web_page[key]
+    return web_page[key[0]]
 
-def get_payload(key, code_name):
+def get_payload(key, code_name, isnew='true', year='105'):
+    # Set 'true' or 'false' to isnew argument that enable history statistics inquery
     payload = {
-        'basic_info':
+        'type_A':
         {
             'encodeURIComponent': '1',
             'step': '1',
@@ -39,7 +45,7 @@ def get_payload(key, code_name):
             'TYPEK': 'all',
             'co_id': code_name,
         },
-        'dividend_distr':
+        'type_B':
         {
             'encodeURIComponent': '1',
             'step': '1',
@@ -52,24 +58,24 @@ def get_payload(key, code_name):
             'queryName': 'co_id',
             'inpuType': 'co_id',
             'TYPEK': 'all',
-            'isnew': 'true',
+            'isnew': isnew,
             'co_id': code_name,
-            'year': '',
+            'year': year,
         },
-        'yearly_eps':
+        'type_C':
         {
             'encodeURIComponent': '1',
             'run': 'Y',
             'step': '1',
             'TYPEK': 'sii',
-            'year': '105',
-            'isnew': 'true',
+            'year': year,
+            'isnew': isnew,
             'co_id': code_name,
             'firstin': '1',
             'off': '1',
             'ifrs': 'Y',
         },
-        'quarterly_eps':
+        'type_D':
         {
             'encodeURIComponent': '1',
             'step': '1',
@@ -84,33 +90,66 @@ def get_payload(key, code_name):
             't05st30_c_ifrs': 'N',
             'inpuType': 'co_id',
             'TYPEK': 'all',
-            'isnew': 'true',
+            'isnew': isnew,
             'co_id': code_name,
-            'year': '',
+            'year': year,
         },
     }
 
     if sys.version_info >= (3,):
-        return urllib.parse.urlencode(payload[key])
+        return urllib.parse.urlencode(payload[key[1]])
     else:
-        return urllib.urlencode(payload[key])
+        return urllib.urlencode(payload[key[1]])
 
 #if __name__ == '__main__':
-def retreive(key, code_name=6414):
-    key_val = dict_maps[key]
+def retreive(opt2key, code_name='6414'):
+    key_val = DICT_MAPS[opt2key]
     s = requests.Session()
-    payload = get_payload(key_val, code_name)
-    resp = s.post(inquiry_web(key_val), data=payload)
-    resp.encoding = 'utf-8'
-    if resp.ok:
-        if key == "NET PROFIT PLUS EPS":
-            # Parsing EPS SUMMARY
-            return get_eps(resp)
-        else:
-            return resp.text
 
-# Proceed parsing...
+    if 'isnew' in get_payload(key_val, code_name):
+        # If there exists a key named isnew in payload, loop begins 2013 (the year gov starts IFRS)
+        str_resp_buf = str()
+        dict_eps = dict()
+        for each_yr in range(102, 107):
+            payload = get_payload(key_val, code_name, 'false', each_yr)
+            resp = s.post(inquiry_web(key_val), data=payload)
+            resp.encoding = 'utf-8'
+            if resp.ok:
+                if opt2key == "NET PROFIT PLUS EPS":
+                    (str_tmp, dict_eps[each_yr + 1911]) = get_eps(resp)
+                    str_resp_buf = str_resp_buf + str_tmp
+                else:
+                    str_resp_buf = str_resp_buf + resp.text
+        #<TODO> Trasfer dict into JSON format and draw on the web by d3.js
+        print(dict_eps)
+        return str_resp_buf
+    else:
+        payload = get_payload(key_val, code_name)
+        resp = s.post(inquiry_web(key_val), data=payload)
+        resp.encoding = 'utf-8'
+        if resp.ok:
+            return resp.text
+            '''
+            if opt2key == "NET PROFIT PLUS EPS":
+                # Parsing EPS SUMMARY
+                return get_eps(resp)
+            else:
+                return resp.text
+            '''
+### Proceed parsing... ###
 def get_eps(resp):
+    try:
+        soup = BeautifulSoup(resp.text, "html.parser")
+        trs = soup.find_all("tr")
+        dict_eps = dict()
+        for idx, each_td in enumerate(trs[-1].find_all('td'), 1):
+            dict_eps[idx] = each_td.get_text().strip()
+        return ("<table>" + trs[0].get_text().strip() + trs[2].get_text().strip() + trs[-1].prettify() + "</table>"), dict_eps
+    except IndexError:
+        return resp.text, []
+'''
+def get_eps(resp):
+#'NET PROFIT PLUS EPS': #['yearly_eps', 'type_C'],
     try:
         soup = BeautifulSoup(resp.text, "html.parser")
         tb = soup.find_all("table")
@@ -120,4 +159,4 @@ def get_eps(resp):
         return reorg_ctnt
     except IndexError:
         return resp.text
-
+'''
