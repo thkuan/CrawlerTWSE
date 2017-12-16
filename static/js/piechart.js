c3153445167f4a7c9c48252f256fdc84be10f396
asdofe <asdofe@gmail.com>
2017-12-16 14:14:36 +0800

    feat(piechart): draw piechart of shareholdings

--- static/js/piechart.js
+++ static/js/piechart.js
@@ -7,79 +7,84 @@
 */
 // Samples
 //
-tmp_arr = json_dataArray[0].shares;
 
-/*var json_dataArray = [
-{"age": "<5", "population": 2704659},
-{"age": "5-13", "population": 4499890},
-{"age": "14-17", "population": 2159981},
-{"age": "18-24", "population": 3853788},
-{"age": "25-44", "population": 14106543},
-{"age": "45-64", "population": 8819342},
-{"age": "≥65", "population": 612463}
-];
-*/
-json_dataArray = [{'age': Object.keys(tmp_arr[1])[0], 'population': parseFloat((tmp_arr[1][Object.keys(tmp_arr[1])][1]).replace(/[^0-9]/g, ""))}];
+var draw_single_piechart = function(param){
+    var this_year = param.year;
+    var annual_sharesholdings = param.dat;
+    //replace(/[^0-9.]/g, "")};});
+    var width = 250,
+        height = 250,
+        radius = Math.min(width, height)/2;
 
-//tmp_arr = tmp_arr.map(function(key1){return {'age': key1, 'population': parseFloat((tmp_arr[key1][1]).replace(/[^0-9]/g, ""))}});
+    var pie_color = d3.scaleOrdinal(d3.schemeCategory20);
+                      //.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-json_dataArray = [];
-tmp_arr.forEach(function(key1, val){
-    json_dataArray.push({'age': Object.keys(key1)[0], 'population': parseFloat(Object.values(key1)[0][1].replace(/[^0-9]/g, ''))});
-});
+    /* Create svg element */
+    var svg = d3.select("body")
+                .append("svg")
+                .attr("height", height)
+                .attr("width", width);
 
-//json_dataArray = 
-//replace(/[^0-9.]/g, "")};});
-var width = 250,
-    height = 250,
-    radius = Math.min(width, height)/2;
 
-var pie_color = d3.scaleOrdinal(d3.schemeCategory20);
-                  //.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-/* Create svg element */
-var svg = d3.select("body")
-            .append("svg")
-            .attr("height", height)
-            .attr("width", width);
+    var graph_width = +svg.attr("width")/2,
+        graph_height = +svg.attr("height")/2;
 
-var graph_width = +svg.attr("width")/2,
-    graph_height = +svg.attr("height")/2;
+    var graph = svg.append("g")
+                .attr("transform", "translate(" + graph_width + "," + graph_height + ")");
 
-var graph = svg.append("g")
-            .attr("transform", "translate(" + graph_width + "," + graph_height + ")");
+    var pie = d3.pie()
+                .sort(null)
+                .value(function(d) {
+                    return d.shares;
+                });
+    var path = d3.arc()
+                    .outerRadius(radius - 40)
+                    .innerRadius(radius - 60);
+    var txt_label = d3.arc()
+                .outerRadius(radius - 20)
+                .innerRadius(radius - 40);
 
-var pie = d3.pie()
-            .sort(null)
-            .value(function(d) {
-                return d.population;
+    var arc = graph.selectAll(".arc")
+                    .data(pie(annual_sharesholdings))
+                    .enter().append("g")
+                    .attr("class", "arc");
+
+
+    arc.append("path")
+            .attr("d", path)
+            .style("fill", function (d) {
+                return pie_color(d.data.owner);
             });
-var path = d3.arc()
-                .outerRadius(radius - 40)
-                .innerRadius(radius - 60);
-var txt_label = d3.arc()
-            .outerRadius(radius - 20)
-            .innerRadius(radius - 40);
 
-var arc = graph.selectAll(".arc")
-                .data(pie(json_dataArray))
-                .enter().append("g")
-                .attr("class", "arc");
+    arc.append("text")
+            .attr("transform", function (d) {
+                return "translate(" + txt_label.centroid(d) + ")";
+            })
+            .attr("dy", ".25em")
+            /* Anchor to middle */
+            .style("text-anchor", "middle")
+            .text(function (d) {
+                return d.data.owner;
+            });
+        graph.append('g').append("text")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("text-anchor", "middle")
+        .style("font-size", "15px")
+        .style("text-decoration", "underline")
+        .text(this_year);
 
-arc.append("path")
-        .attr("d", path)
-        .style("fill", function (d) {
-            return pie_color(d.data.age);
-        });
 
-arc.append("text")
-        .attr("transform", function (d) {
-            return "translate(" + txt_label.centroid(d) + ")";
-        })
-        .attr("dy", ".25em")
-        /* Anchor to middle */
-        .style("text-anchor", "middle")
-        .text(function (d) {
-            return d.data.age;
-        });
-
+}
+var dispatch_annual_piechart = function (ele, idx) {
+    annual_shares = ele.shares.map(function (share, sidx) {
+        trim_share = Object.values(share)[0][1];
+        trim_share = trim_share.replace(/[^0-9.]/g, "");
+        trim_share = parseFloat(trim_share);
+        owner_name = Object.keys(share)[0];
+        return {owner : owner_name, shares : trim_share};
+    });
+    draw_single_piechart({year: ele.year, dat : annual_shares});
+}
+json_dataArray.map(dispatch_annual_piechart);
